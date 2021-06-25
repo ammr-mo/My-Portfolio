748f83ce322314ce2fb3f4c55d1b78b2f9e553b4
Ammran <ahm2169@columbia.edu>
2021-06-25 17:58:10 +0000

    add contact box and move photos around

--- portfolio/src/main/java/com/google/sps/servlets/InterestsServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/InterestsServlet.java
@@ -1,10 +1,13 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import java.util.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
 
 /** Handles requests sent to the /interests URL. Try running a server and navigating to /interests! */
 @WebServlet("/interests")
@@ -12,7 +15,14 @@ public class InterestsServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("I am particularly interesting in Natural Language Processing and want to learn more about Translators!");
+    ArrayList<String> myInterests = new ArrayList<String>();
+    myInterests.add("I am interested in AI and want to learn more about Natural Language Processing.");
+    myInterests.add("My favorite color is Orange.");
+    myInterests.add("I am from Minnesota, and my family is Somali.");
+
+    Gson gson = new Gson();
+    String json = gson.toJson(myInterests);
+    response.setContentType("text/json;");
+    response.getWriter().println(json);
   }
 }
